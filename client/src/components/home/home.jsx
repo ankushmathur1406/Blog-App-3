0e996ee55f9fa1b57e712f5a2f6239a1c2bf6d50
ankushmathur1406 <mathurankush1406@gmail.com>
2024-10-19 12:42:54 +0530

    1st commit -client added.

---
+++ client/src/components/home/home.jsx
@@ -0,0 +1,26 @@
+
+import { Grid2 } from '@mui/material';
+
+//components
+import Banner from '../banner/Banner';
+import Categories from './Categories';
+import Posts from './post/Posts';
+
+const Home = () => {
+
+    return (
+        <>
+            <Banner />
+            <Grid2 container>
+                <Grid2 item lg={2} xs={12} sm={2}>
+                    <Categories />
+                </Grid2>
+                <Grid2 container item xs={12} sm={10} lg={10}>
+                    <Posts />
+                </Grid2>
+            </Grid2>
+        </>
+    )
+}
+
+export default Home;
