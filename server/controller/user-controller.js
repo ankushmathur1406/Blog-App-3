55c62adb844f705781e5e676f3289edf99d99e22
ankushmathur1406 <mathurankush1406@gmail.com>
2024-11-12 23:37:37 +0530

    changes2

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,61 +1,62 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import bcryptjs from 'bcryptjs';
-import Token from '../model/token.js'
-import User from '../model/user.js';
+// import bcrypt from 'bcrypt';
+// import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+// import bcryptjs from 'bcryptjs';
+// import Token from '../model/token.js'
 
-dotenv.config();
+// import User from '../model/user.js';
 
-export const signupUser = async (request, response) => {
-    try {
-        // const salt = await bcrypt.genSalt();
-        // const hashedPassword = await bcrypt.hash(request.body.password, salt);
-        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+// dotenv.config();
 
-        const user = { username: request.body.username, name: request.body.name, password: hashedPassword }
+// export const signupUser = async (request, response) => {
+//     try {
+//         // const salt = await bcrypt.genSalt();
+//         // const hashedPassword = await bcrypt.hash(request.body.password, salt);
+//         const hashedPassword = await bcrypt.hash(request.body.password, 10);
 
-        const newUser = new User(user);
-        await newUser.save();
+//         const user = { username: request.body.username, name: request.body.name, password: hashedPassword }
 
-        return response.status(200).json({ msg: 'Signup successfull' });
-    } catch (error) {
-        return response.status(500).json({ msg: 'Error while signing up user' });
-    }
-}
+//         const newUser = new User(user);
+//         await newUser.save();
+
+//         return response.status(200).json({ msg: 'Signup successfull' });
+//     } catch (error) {
+//         return response.status(500).json({ msg: 'Error while signing up user' });
+//     }
+// }
 
 
-export const loginUser = async (request, response) => {
-    let user = await User.findOne({ username: request.body.username });
-    if (!user) {
-        return response.status(400).json({ msg: 'Username does not match' });
-    }
+// export const loginUser = async (request, response) => {
+//     let user = await User.findOne({ username: request.body.username });
+//     if (!user) {
+//         return response.status(400).json({ msg: 'Username does not match' });
+//     }
 
-    try {
-        let match = await bcrypt.compare(request.body.password, user.password);
-        if (match) {
-            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
-            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+//     try {
+//         let match = await bcrypt.compare(request.body.password, user.password);
+//         if (match) {
+//             const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+//             const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
             
-            const newToken = new Token({ token: refreshToken });
-            await newToken.save();
+//             const newToken = new Token({ token: refreshToken });
+//             await newToken.save();
         
-            response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
+//             response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
         
-        } else {
-            response.status(400).json({ msg: 'Password does not match' })
-        }
-    } catch (error) {
-        response.status(500).json({ msg: 'error while login the user' })
-    }
-}
+//         } else {
+//             response.status(400).json({ msg: 'Password does not match' })
+//         }
+//     } catch (error) {
+//         response.status(500).json({ msg: 'error while login the user' })
+//     }
+// }
 
-export const logoutUser = async (request, response) => {
-    const token = request.body.token;
-    await Token.deleteOne({ token: token });
+// export const logoutUser = async (request, response) => {
+//     const token = request.body.token;
+//     await Token.deleteOne({ token: token });
 
-    response.status(204).json({ msg: 'logout successfull' });
-}
+//     response.status(204).json({ msg: 'logout successfull' });
+// }
 
 
 // import crypto from 'crypto';
@@ -125,3 +126,58 @@ export const logoutUser = async (request, response) => {
 
 //     response.status(204).json({ msg: 'Logout successful' });
 // }
+
+
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import Token from '../model/token.js';
+import User from '../model/user.js';
+
+dotenv.config();
+
+export const signupUser = async (request, response) => {
+    try {
+        const hashedPassword = await argon2.hash(request.body.password);
+
+        const user = { username: request.body.username, name: request.body.name, password: hashedPassword };
+
+        const newUser = new User(user);
+        await newUser.save();
+
+        return response.status(200).json({ msg: 'Signup successful' });
+    } catch (error) {
+        return response.status(500).json({ msg: 'Error while signing up user' });
+    }
+};
+
+export const loginUser = async (request, response) => {
+    let user = await User.findOne({ username: request.body.username });
+    if (!user) {
+        return response.status(400).json({ msg: 'Username does not match' });
+    }
+
+    try {
+        let match = await argon2.verify(user.password, request.body.password);
+        if (match) {
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m' });
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            
+            const newToken = new Token({ token: refreshToken });
+            await newToken.save();
+
+            response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username });
+        } else {
+            response.status(400).json({ msg: 'Password does not match' });
+        }
+    } catch (error) {
+        response.status(500).json({ msg: 'Error while logging in the user' });
+    }
+};
+
+export const logoutUser = async (request, response) => {
+    const token = request.body.token;
+    await Token.deleteOne({ token: token });
+
+    response.status(204).json({ msg: 'Logout successful' });
+};
